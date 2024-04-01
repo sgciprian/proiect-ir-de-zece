ba37d009426c6834e73a96327eee0605b6ed6151
Ciprian Stanciu <g.c.stanciu@student.tudelft.nl>
2024-04-01 15:17:45 +0300

    progress indicator

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import json
 from engine import Engine
 
 
-LIMIT_INSTANCES = 10
+LIMIT_INSTANCES = 5
 PRINT_INSTANCES = True
 
 DATASET_PATH = "data/test_claims_quantemp.json"
@@ -24,6 +24,8 @@ e = Engine(EVIDENCE_PATH, RERANKING_MODEL, VERACITY_MODEL)
 with open(DATASET_PATH, 'r') as j:
     data = json.load(j)
 
+tests_max = min(len(data), LIMIT_INSTANCES)
+
 tests_count = 0
 tests_matching = 0
 binary_true_positive = 0
@@ -35,15 +37,25 @@ for instance in data:
     if tests_count >= LIMIT_INSTANCES:
         break
 
-    computed_verdict = e.verify(instance["claim"])
-    real_verdict = VERDICT_MAP[instance["label"]]
+    if not PRINT_INSTANCES:
+        work_done = tests_count / tests_max
+        print("\rProgress: [{0:50s}] {1}%".format('#' * int(work_done * 50), work_done * 100), end="", flush=True)
 
-    if PRINT_INSTANCES:
-        print(f"Computed: {computed_verdict[0]}")
-        print(f"Real: {real_verdict}")
-        print(f"Evidence: {computed_verdict[1]}")
+    computed = e.verify(instance["claim"])
+    computed_verdict = computed[0]
+    real_verdict = VERDICT_MAP[instance["label"]]
+    evidence = computed[1]
 
     tests_count += 1
+
+    if PRINT_INSTANCES:
+        print(f"{tests_count} / {tests_max}")
+        print(f"Claim: {instance["claim"]}")
+        print(f"Computed: {computed_verdict}")
+        print(f"Real: {real_verdict}")
+        print(f"Evidence: {evidence}")
+        print()
+
     if computed_verdict == real_verdict:
         tests_matching += 1
     if computed_verdict != "neutral" and real_verdict != "neutral":
@@ -60,7 +72,8 @@ for instance in data:
 
 print(f"{tests_matching} correctly classified in {tests_count} test instances.")
 print(f"")
-print(f"For entailment/contradiction labels:")
+print(f"{binary_true_positive + binary_true_negative + binary_false_positive + binary_false_negative} entailment"
+      f"/contradiction labels")
 print(f"{binary_true_positive} true positives")
 print(f"{binary_true_negative} true negatives")
 print(f"{binary_false_positive} false positives")
